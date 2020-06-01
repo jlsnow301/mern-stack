f03b875b285cb9276c4760a7fccd7a95d8a94c76
Jerm <42397676+jlsnow301@users.noreply.github.com>
2020-06-01 04:28:49 -0700

    New Header Functionality

--- client/src/user/components/UsersList.js
+++ client/src/user/components/UsersList.js
@@ -1,20 +1,23 @@
 import React from "react";
-import UserItem from "./UserItem";
 
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 import "./UsersList.css";
 
 const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
 
   return (
-    <ul>
-      {props.item.map((user) => (
+    <ul className="users-list">
+      {props.items.map((user) => (
         <UserItem
           key={user.id}
           id={user.id}
