8f9bcd4597080b895b46f69f6b731824048eab62
Jerm <42397676+jlsnow301@users.noreply.github.com>
2020-06-08 17:56:45 -0700

    Form Functionality Nearly Complete

--- client/src/places/pages/UserPlaces.js
+++ client/src/places/pages/UserPlaces.js
@@ -9,7 +9,7 @@ const DUMMY_PLACES = [
     title: "Empire State Building",
     description: "One of the most famous skyscrapers!",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Empire_States_Building.jpg/250px-Empire_States_Building.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
     address: "20 W 34th St, New York, NY 10001",
     location: {
       lat: 40.7484405,
@@ -22,7 +22,7 @@ const DUMMY_PLACES = [
     title: "Empire State Building",
     description: "One of the most famous skyscrapers!",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Empire_States_Building.jpg/250px-Empire_States_Building.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
     address: "20 W 34th St, New York, NY 10001",
     location: {
       lat: 40.7484405,
--- client/src/shared/components/FormElements/Input.js
+++ client/src/shared/components/FormElements/Input.js
@@ -1,5 +1,6 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
+import { validate } from "../../util/validators";
 import "./Input.css";
 
 const inputReducer = (state, action) => {
@@ -8,8 +9,14 @@ const inputReducer = (state, action) => {
       return {
         ...state,
         value: action.val,
-        isValid: true,
+        isValid: validate(action.val, action.validators),
       };
+    case "TOUCH": {
+      return {
+        ...state,
+        isTouched: true,
+      };
+    }
     default:
       return state;
   }
@@ -17,12 +24,30 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: "",
-    isValid: false,
+    value: props.initialValue || "",
+    isTouched: false,
+    isValid: props.initialValid || false,
   });
 
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
   const changeHandler = (event) => {
-    dispatch({ type: "CHANGE", val: event.target.value });
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: "TOUCH",
+    });
   };
 
   const element =
@@ -31,6 +56,8 @@ const Input = (props) => {
         id={props.id}
         type={props.type}
         placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
         value={inputState.value}
       />
     ) : (
@@ -38,6 +65,7 @@ const Input = (props) => {
         id={props.id}
         rows={props.rows || 3}
         onChange={changeHandler}
+        onBlur={touchHandler}
         value={inputState.value}
       />
     );
@@ -45,12 +73,12 @@ const Input = (props) => {
   return (
     <div
       className={`form-control ${
-        !inputState.isValid && "form-control--invalid"
+        !inputState.isValid && inputState.isTouched && "form-control--invalid"
       }`}
     >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
-      {!inputState.isValid && <p>{props.errorText}</p>}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
     </div>
   );
 };
