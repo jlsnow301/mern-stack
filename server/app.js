38ecdd2b7218f393ba4eba91a1df0640689923a9
Jerm <42397676+jlsnow301@users.noreply.github.com>
2020-06-24 22:26:05 -0700

    MongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -9,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/places", placesRoutes); // => /api/places...
+app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
@@ -25,4 +26,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured." });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb+srv://Jerm:rnv5D929HUXIVdpQ@cluster0-bywu5.mongodb.net/places?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
