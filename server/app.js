1517137c129130c6fd1830c0386f9090fed2a7c5
Jerm <42397676+jlsnow301@users.noreply.github.com>
2020-06-25 19:53:07 -0700

    Linking Front End

--- server/app.js
+++ server/app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -27,9 +37,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://Jerm:rnv5D929HUXIVdpQ@cluster0-bywu5.mongodb.net/places?retryWrites=true&w=majority"
-  )
+  .connect("MONGOURI")
   .then(() => {
     app.listen(5000);
   })
